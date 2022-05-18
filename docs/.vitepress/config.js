c0e31f253a275e6687f30893ad18a4a89ed0f3c2
upmin-l <lanshikeng@uinnova.com>
2022-05-18 21:55:32 +0800

    feat: 初始化

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: 'xxv-cli',
+    description: 'welcome',
+    smoothScroll: true,
+    themeConfig: {
+        nav: [
+            { text: '文档', link: '/guide/' },
+            { text: 'API 参考', link: '/api/' },
+            {
+                text: '更新日志',
+                link:
+                    'https://github.com/xinlei3166/vitepress-demo'
+            }
+        ]
+    }
+}
